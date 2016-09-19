5a63ee6fd74d95a6d9afa5ff2040fdb6102fd62d
mwnachman <mwnachman@gmail.com>
2016-09-19 16:44:38 -0700

    "Saving to we can make changes to go back to.  We have dots appearing in random places, but they are not blinking"

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,13 +5,13 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
     expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
   });
-
+ 
   it('should have a step function that makes its node blink', function() {
     sinon.spy(blinkyDancer.$node, 'toggle');
     blinkyDancer.step();
