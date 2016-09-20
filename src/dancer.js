4d1e83941a8b0c8a5c8fe3ac9b0ff2fbcab7c864
mwnachman <mwnachman@gmail.com>
2016-09-19 19:00:27 -0700

    Refactored makerDancer and makerBlinkyDancer to be pseuodoclassical and passed all tests.

--- src/dancer.js
+++ src/dancer.js
@@ -10,14 +10,13 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
-
 };
 
 
 makeDancer.prototype.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 
@@ -25,7 +24,6 @@ makeDancer.prototype.step = function() {
 makeDancer.prototype.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
-    //
   var styleSettings = {
     top: top,
     left: left
--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 
   $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
+     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
      * class="addDancerButton" DOM node matches one of the names of the
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-    console.log('danceMakerFunction', dancerMakerFunction);
+    // console.log('danceMakerFunction', dancerMakerFunction);
 
     // make a dancer with a random position
 
@@ -29,6 +29,8 @@ $(document).ready(function() {
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+
+
 
     $('body').append(dancer.$node);
 
